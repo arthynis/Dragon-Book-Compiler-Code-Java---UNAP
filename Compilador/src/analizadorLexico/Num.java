d5f53a9cf1f330b7acb7fcd5d8b65778ef75e525
arthynis <77287181+arthynis@users.noreply.github.com>
2021-01-15 10:06:23 -0500

    Delete Compilador directory

--- Compilador/src/analizadorLexico/Num.java
+++
@@ -1,7 +0,0 @@
-package analizadorLexico;           //Archivo Num.java
-public class Num extends Token {
-    public final int valor;
-    public Num(int v) {super(Etiqueta.NUM); valor = v;}
-    public String toString() { return "" + valor;}
-}
-
--- Compilador/src/analizadorLexico/Token.java
+++
@@ -1,7 +0,0 @@
-package analizadorLexico;       //Arichivo Token.java
-public class Token {
-    public final int etiqueta;
-    public Token(int t) {etiqueta = t;}
-    public String toString() {return "" + (char)etiqueta;}
-}
-
